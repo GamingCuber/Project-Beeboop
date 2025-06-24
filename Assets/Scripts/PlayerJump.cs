5ac828bcb5b5854efd8622ec4f15abada04ad7e8
Kevin-Siu <kevin.siu007@gmail.com>
2025-06-23 18:55:31 -0700

    Added jump, grav manager
    Added hella parameters for playerdata too
    
    amogus scene is mine aint none of you mess with it or you catching hands

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,128 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private bool grounded = false;
+
+    private bool canJump = true;
+
+    private int jumpsLeft;
+
+    private Coroutine jumpCo;
+
+    private Coroutine coyoteCo;
+
+    private LayerMask platformLayer;
+
+    private void Start()
+    {
+        platformLayer = LayerMask.GetMask("Platform");
+        StartCoroutine(waitForData());
+    }
+
+    void Update()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 1.05f, platformLayer)) //if player is close to ground theyre grounded!
+        {
+            canJump = true;
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        if (!grounded && canJump && coyoteCo == null && jumpsLeft == PlayerDataManager.Instance.getData().jumpAmt) //gives coyote time if they were recently on the ground, they isn't already a co running and if theyre on their first jump
+        {
+            coyoteCo = StartCoroutine(coyoteTimer());
+        }
+
+        if (Input.GetKeyDown(PlayerInputs.Instance.jump) && jumpsLeft > 0 && canJump)
+        {
+            jump();
+        }
+    }
+
+    private void jump() //do a jump, cuz this functions gonna get called in more than one places
+    {
+        jumpsLeft--;
+        cancelJump();
+        jumpCo = StartCoroutine(doJump());
+    }
+
+    private IEnumerator doJump() //itll addforce and watch for conditions to mess with the gravity
+    {
+        rb.AddForceY(PlayerDataManager.Instance.getData().jumpStr, ForceMode2D.Impulse);
+
+        while (true)
+        {
+            if (!Input.GetKey(PlayerInputs.Instance.jump) || rb.linearVelocityY < -0.5) //if they let go or player starts falling, increase grav so they fall faster
+            {
+                PlayerGravManager.Instance.setGrav(PlayerDataManager.Instance.getData().jumpFallGrav);
+                break;
+            }
+            else if (Mathf.Abs(rb.linearVelocityY) < 1.5) //if player is at the peak of their jump, so when the abs velo is low
+            {
+                PlayerGravManager.Instance.setGrav(PlayerDataManager.Instance.getData().jumpFloatGrav);
+            }
+            else
+            {
+                PlayerGravManager.Instance.resetGrav();
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        while (!grounded) //just stall till the player hits the ground to reset grav
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        PlayerGravManager.Instance.resetGrav();
+        resetJumps();
+        yield break;
+    }
+
+    private IEnumerator coyoteTimer() //counts frames for when the player can jump off whilst not grounded
+    {
+        int count = 0;
+
+        while (count < PlayerDataManager.Instance.getData().coyoteFrames)
+        {
+            count++;
+            yield return new WaitForEndOfFrame();
+        }
+
+        canJump = false;
+        coyoteCo = null;
+        yield break;
+    }
+
+    private void resetJumps() //gives jumpsleft back
+    {
+        jumpsLeft = PlayerDataManager.Instance.getData().jumpAmt;
+    }
+
+    private void cancelJump() //resets grav, player y velo, redoes the jump coroutine
+    {
+        if (jumpCo != null)
+        {
+            StopCoroutine(jumpCo);
+        }
+
+        rb.linearVelocityY = 0;
+        PlayerGravManager.Instance.resetGrav();
+    }
+
+    private IEnumerator waitForData() //wait for the data manager singleton to be initialized
+    {
+        while (PlayerDataManager.Instance == null)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        resetJumps();
+    }
+}
