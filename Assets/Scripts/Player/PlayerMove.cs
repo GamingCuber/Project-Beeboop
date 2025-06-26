b77baef698fc8d5666d2d3bdacfe6b91c2b1b682
Kevin-Siu <kevin.siu007@gmail.com>
2025-06-25 21:39:43 -0700

    Worked on movement
    Added speed cap
    Made it so your momentum saves when you change direction

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,37 +4,52 @@ public class PlayerMove : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+    private int dir; //left or right, -1 left, 1 right
+
     void Update()
     {
-        if (!PlayerStateManager.Instance.getState().isHooked)
+        if (!PlayerStateManager.Instance.getState().isHooked && !PlayerStateManager.Instance.getState().isDashing)
         {
-            if (Input.GetKey(PlayerInputs.Instance.right))
+            if (Input.GetKey(PlayerInputs.Instance.left) || Input.GetKey(PlayerInputs.Instance.right))
             {
-                PlayerDataManager.Instance.getData().playerdirection = "right";
-                rb.AddForceX(PlayerDataManager.Instance.getData().playerAcc, ForceMode2D.Force);
+                float xVelo = rb.linearVelocityX;
 
-            }
+                float maxSpd = PlayerDataManager.Instance.getData().playerMaxSpd;
 
-            else if (Input.GetKey(PlayerInputs.Instance.left))
-            {
-                PlayerDataManager.Instance.getData().playerdirection = "left";
-                rb.AddForceX(-PlayerDataManager.Instance.getData().playerAcc, ForceMode2D.Force);
+                float AFMult = 1; //add force mult, so i can make it close to 0 if the player is close to max speed
 
+                if (Mathf.Abs(xVelo) > maxSpd - maxSpd/4) //so if the player is approaching their max speed
+                {
+                    AFMult = 1 - (Mathf.Abs(xVelo) / maxSpd);
+                }
+
+                if (Input.GetKey(PlayerInputs.Instance.left))
+                {
+                    if (PlayerDataManager.Instance.getData().playerdirection == "right")
+                    {
+                        rb.linearVelocityX *= -1;
+                    }
+
+                    dir = -1;
+                    PlayerDataManager.Instance.getData().playerdirection = "left";
+                }
+                else
+                {
+                    if (PlayerDataManager.Instance.getData().playerdirection == "left")
+                    {
+                        rb.linearVelocityX *= -1;
+                    }
+
+                    dir = 1;
+                    PlayerDataManager.Instance.getData().playerdirection = "right";
+                }
+
+                rb.AddForceX(PlayerDataManager.Instance.getData().playerAcc * AFMult * dir, ForceMode2D.Force);
             }
             else
             {
-                Vector2 velo = rb.linearVelocity; // G
-                if (PlayerStateManager.Instance.getState().isDashing == false)
-                {
-
-                    velo.x = 0;
-                }
-
-
-
-                rb.linearVelocity = velo;
+                rb.linearVelocityX = 0;
             }
         }
     }
 }
-
