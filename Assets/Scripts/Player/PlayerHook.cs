a6ae731b24c1f5e7635bab842aa05b1162c39244
Enkhamgalan Geoffrey Enkhsaikhan <Thegamingcuber19@gmail.com>
2025-06-26 13:48:17 -0700

    Hook now updates line renderer every frame

--- Assets/Scripts/Player/PlayerHook.cs
+++ Assets/Scripts/Player/PlayerHook.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using Mono.Cecil.Cil;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerHook : MonoBehaviour
 {
@@ -14,28 +15,43 @@ public class PlayerHook : MonoBehaviour
 
     void Update()
     {
+        GameObject hook = getClosestHook();
         if (Input.GetKeyDown(PlayerInputs.Instance.hook))
         {
-            if (!PlayerStateManager.Instance.getState().isHooked)
-            {
-                // Resets player force for a frame, then continues movement in the next line
-                rb.constraints = RigidbodyConstraints2D.FreezeAll;
-            }
-            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-            PlayerStateManager.Instance.getState().isHooked = true;
-            GameObject hook = getClosestHook();
-            hookLineRenderer.enabled = true;
 
+
+            if (hook != null)
+            {
+                if (Vector2.Distance(hook.transform.position, transform.position) < hookDistanceLimit)
+                {
+                    if (!PlayerStateManager.Instance.getState().isHooked)
+                    {
+                        // Resets player force for a frame, then continues movement in the next line
+                        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+                    }
+                    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                    PlayerStateManager.Instance.getState().isHooked = true;
+
+                    hookLineRenderer.enabled = true;
+
+                    // Calculates Vector between player and HookPoint for applying Force
+                    Vector2 hookVector = (hook.transform.position - transform.position) * Time.fixedDeltaTime;
+                    rb.AddForce(hookVector * hookSpeed, ForceMode2D.Impulse);
+                }
+
+            }
+        }
+
+        if (Input.GetKey(PlayerInputs.Instance.hook) && PlayerStateManager.Instance.getState().isHooked)
+        {
             // Draws line between Player and Hook Point
             hookLineRenderer.SetPosition(0, transform.position);
             hookLineRenderer.SetPosition(1, hook.transform.position);
-
-            // Calculates Vector between player and HookPoint for applying Force
-            UnityEngine.Vector2 hookVector = (hook.transform.position - transform.position) * Time.fixedDeltaTime;
-            rb.AddForce(hookVector * hookSpeed, ForceMode2D.Impulse);
         }
+
         if (Input.GetKeyUp(PlayerInputs.Instance.hook))
         {
+            rb.inertia = 5;
             rb.constraints = RigidbodyConstraints2D.FreezePositionY;
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             PlayerStateManager.Instance.getState().isHooked = false;
@@ -53,8 +69,8 @@ public class PlayerHook : MonoBehaviour
             closestHook = hookObjects[0];
             foreach (var hookObject in hookObjects)
             {
-                var currentHookDistanceFromPlayer = Mathf.Abs(UnityEngine.Vector2.Distance(transform.position, hookObject.transform.position));
-                var closestHookDistanceFromPlayer = Mathf.Abs(UnityEngine.Vector2.Distance(transform.position, closestHook.transform.position));
+                var currentHookDistanceFromPlayer = Mathf.Abs(Vector2.Distance(transform.position, hookObject.transform.position));
+                var closestHookDistanceFromPlayer = Mathf.Abs(Vector2.Distance(transform.position, closestHook.transform.position));
 
                 if (currentHookDistanceFromPlayer <= closestHookDistanceFromPlayer)
                 {
