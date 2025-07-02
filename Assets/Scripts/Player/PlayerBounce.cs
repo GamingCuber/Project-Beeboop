c738fb443d745bceefb247bb15b2a0788ae7af92
Enkhamgalan Geoffrey Enkhsaikhan <Thegamingcuber19@gmail.com>
2025-07-02 14:30:15 -0700

    Bounce Pad Functional, fixing jump state management

--- Assets/Scripts/Player/PlayerBounce.cs
+++ Assets/Scripts/Player/PlayerBounce.cs
@@ -7,18 +7,17 @@ public class PlayerBounce : MonoBehaviour
 
     void Start()
     {
-        platformLayer = LayerMask.NameToLayer("Platform");
+        platformLayer = LayerMask.GetMask("Platform");
     }
     void Update()
     {
         if (Physics2D.Raycast(transform.position, Vector2.down, 1.01f, platformLayer))
         {
-            Debug.Log("bouning");
-
             GameObject bottomPlatform = Physics2D.Raycast(transform.position, Vector2.down, 1.01f, platformLayer).collider.gameObject;
 
             if (bottomPlatform != null && bottomPlatform.CompareTag("BouncePlatform"))
             {
+                Debug.Log("bouncing");
                 rb.AddForce(Vector2.up * PlayerDataManager.Instance.getData().bounceForce);
             }
         }
