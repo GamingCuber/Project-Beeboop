6624a37910323c755c047d49b201ff524eaf573a
Kevin-Siu <kevin.siu007@gmail.com>
2025-07-09 17:18:51 -0700

    Added gear wiggle

--- Assets/Scripts/UI/TimerGear.cs
+++ Assets/Scripts/UI/TimerGear.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class TimerGear : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class TimerGear : MonoBehaviour
 
     public float maxSpeed;
 
+    public bool paused;
+
     private float speed;
 
     public float dir; //-1 or 1 please
+
+    public float maxWiggle;
+
+    public int wiggleAmt;
 
     void Update()
     {
@@ -20,5 +27,59 @@ public class TimerGear : MonoBehaviour
     public void setSpeed(float speed)
     {
         this.speed = speed;
+    }
+
+    public void vibrateGear()
+    {
+        StartCoroutine(vibrateCo());
+    }
+
+    private IEnumerator vibrateCo()
+    {
+        paused = true;
+        setSpeed(0);
+
+        int timesMoved = 0;
+
+        float timer = 0;
+
+        float timeToMove = 0.1f;
+
+        RectTransform rect = this.gameObject.GetComponent<RectTransform>();
+
+        Vector3 ogPos = rect.localPosition;
+
+        Vector3 curPos = ogPos;
+
+        Vector3 newPos = randomizePos(ogPos);
+
+        while (timesMoved < wiggleAmt)
+        {
+            timer += Time.deltaTime;
+
+            float x = Mathf.Lerp(curPos.x, newPos.x, timer / timeToMove);
+            float y = Mathf.Lerp(curPos.y, newPos.y, timer / timeToMove);
+
+            rect.localPosition = new Vector3(x, y, 0);
+
+            if (timer >= timeToMove)
+            {
+                timer = 0;
+                timeToMove = 0.1f;
+                curPos = newPos;
+                newPos = randomizePos(ogPos);
+                timesMoved++;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        paused = false;
+        yield break;
+    }
+
+    private Vector3 randomizePos(Vector3 pos)
+    {
+        return pos + Vector3.up * Random.Range(-maxWiggle, maxWiggle) + Vector3.left * Random.Range(-maxWiggle, maxWiggle);
     }
 }
