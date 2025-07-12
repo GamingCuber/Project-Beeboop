7ad76a25bd85fa0dffa8ce855aed3c657b7b69a2
Kevin-Siu <kevin.siu007@gmail.com>
2025-07-12 12:23:20 -0700

    Added conveyor

---
+++ Assets/Scripts/Player/PlayerConveyor.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerConveyor : MonoBehaviour
+{
+    private bool onConveyor = false;
+
+    private ConveyorPlatform curConvey;
+
+    private Coroutine conveyCo;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Conveyor"))
+        {
+            curConvey = collision.gameObject.GetComponent<ConveyorPlatform>();
+            attachPlayer();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (onConveyor)
+        {
+            detachPlayer();
+        }
+    }
+
+    private void attachPlayer()
+    {
+        onConveyor = true;
+        StartCoroutine(movePlayer());
+    }
+
+    private void detachPlayer()
+    {
+        onConveyor = false;
+        curConvey = null;
+
+        if (conveyCo != null)
+        {
+            StopCoroutine(conveyCo);
+        }
+
+        conveyCo = null;
+    }
+
+    private IEnumerator movePlayer()//GOTTA FIX
+    {
+        float speed = curConvey.speed;
+        float dir = curConvey.dir;
+
+        while (onConveyor)
+        {
+            Vector3 targetPos = transform.position + Vector3.right * dir * speed * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, 1);
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
