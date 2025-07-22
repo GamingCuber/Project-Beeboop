573d1cfd99d9f34bea888a31e3919b8e2309613b
Enkhamgalan Geoffrey Enkhsaikhan <Thegamingcuber19@gmail.com>
2025-07-22 14:50:47 -0700

    Added Player Left and Right

--- Assets/Scripts/Player/AnimationController.cs
+++ Assets/Scripts/Player/AnimationController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimationController : MonoBehaviour
 {
@@ -8,11 +9,12 @@ public class AnimationController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(PlayerInputs.Instance.left) && !sr.flipX) //if they goin left, flip sprite, otherwise don't
+
+        if ((Input.GetKey(PlayerInputs.Instance.left) && !sr.flipX) || (Input.GetAxis("Horizontal") < 0 && !sr.flipX)) //if they goin left, flip sprite, otherwise don't
         {
             sr.flipX = true;
         }
-        else if (Input.GetKey(PlayerInputs.Instance.right) && sr.flipX)
+        else if ((Input.GetKey(PlayerInputs.Instance.right) && sr.flipX) || (Input.GetAxis("Horizontal") > 0 && sr.flipX))
         {
             sr.flipX = false;
         }
