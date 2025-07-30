f16c61fd3064d368bddf3688036f0720bb102906
Kevin-Siu <kevin.siu007@gmail.com>
2025-07-29 22:41:51 -0700

    Level Transitions

---
+++ Assets/Scripts/Gameplay/LevelTransitionManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitionManager : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private Collider2D coll;
+
+    private void Start()
+    {
+        coll = this.GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            LevelTransition.Instance.doTransition(nextSceneName);
+        }
+    }
+}
