45f533d9121a92af11972963b2f9a1f1312e3b4a
Kevin-Siu <kevin.siu007@gmail.com>
2025-08-09 19:34:54 -0700

    Camera changes

--- Assets/Scripts/Gameplay/CameraFollow.cs
+++ Assets/Scripts/Gameplay/CameraFollow.cs
@@ -7,12 +7,20 @@ public class CameraFollow : MonoBehaviour
     public float minSpeed;
     public float maxSpeed;
     public float yOffset;
+    public float xDirOffset; //how far forward the camera is toward the direction the players looking
 
     void FixedUpdate()
     {
         float speed = minSpeed;
 
-        float dis = Vector2.Distance(this.transform.position, player.transform.position) - yOffset; //distance btwn player and cam
+        float dir = 1;
+
+        if (PlayerDataManager.Instance.getData().playerDirection == "left")
+        {
+            dir = -1;
+        }
+
+        float dis = Vector2.Distance(this.transform.position + Vector3.down * yOffset - Vector3.right * dir * xDirOffset, player.transform.position); //distance btwn player and cam
 
         if (dis < distanceScale)
         {
@@ -21,9 +29,13 @@ public class CameraFollow : MonoBehaviour
         else
         {
             speed = maxSpeed;
-        } 
+        }
 
-        Vector3 newPos = Vector3.MoveTowards(transform.position, player.transform.position + new Vector3(0f, yOffset, 0f), speed * Time.deltaTime);
+        Debug.Log(speed);
+
+        Vector3 targetPos = player.transform.position + Vector3.right * dir * xDirOffset;
+
+        Vector3 newPos = Vector3.MoveTowards(transform.position, targetPos + new Vector3(0f, yOffset, 0f), speed * Time.deltaTime);
         newPos.z = -10;
         this.transform.position = newPos;
     }
