925125710a3b9730defb8672c79ccab95f9be997
Kevin-Siu <kevin.siu007@gmail.com>
2025-08-14 17:09:25 -0700

    Death sound?

--- Assets/Scripts/Player/SpikesCollided.cs
+++ Assets/Scripts/Player/SpikesCollided.cs
@@ -11,6 +11,7 @@ public class SpikesCollided : MonoBehaviour
             PlayerJump.Instance.cancelJump(false);
             DashAfterimage.Instance.cancelAfterImage();
             DeathAnimManager.Instance.doAnimation();
+            SoundManager.Instance.playPlayerSound("death");
             gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
             Invoke(nameof(onDeath), 1f);
         }
