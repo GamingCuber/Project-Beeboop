92ac53ff674eada5a22f601b8a40addff63ce75a
Kevin <kevin.siu007@gmail.com>
2026-07-08 15:10:26 -0700

    Finished tutorial popup

--- Assets/Scripts/UI/TutorialPopupManager.cs
+++ Assets/Scripts/UI/TutorialPopupManager.cs
@@ -5,6 +5,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
+using UnityEngine.Playables;
 
 public class TutorialPopupManager : MonoBehaviour
 {
@@ -25,16 +26,23 @@ public class TutorialPopupManager : MonoBehaviour
     public static TutorialPopupManager Instance;
     private WaitForEndOfFrame wait = new WaitForEndOfFrame();
     public dataEnum[] dataOptions;
+    
     private GameObject upgradePopUp;
     private TMP_Text descriptionText;
     private VideoPlayer upgradeVideo;
     private Image inputIcon;
     private TMP_Text inputText;
+    private TMP_Text tabText;
 
     private RectTransform panelTransform;
     public float moveTime;
     public Vector3 hiddenPosition;
     public Vector3 showingPosition;
+
+    private PlayableDirector director;
+    public PlayableAsset show;
+    public PlayableAsset hide;
+    private TutorialData curData;
 
     void Start()
     {
@@ -44,14 +52,21 @@ public class TutorialPopupManager : MonoBehaviour
         }
 
         upgradePopUp = GameObject.FindGameObjectWithTag("UpgradePopUp");
-        descriptionText = upgradePopUp.transform.Find("DescriptionText").GetComponent<TMP_Text>();
+
+        GameObject tutorialBody = upgradePopUp.transform.Find("Mask").Find("TutorialBody").gameObject;
+
+        descriptionText = tutorialBody.transform.Find("DescriptionText").GetComponent<TMP_Text>();
         upgradeVideo = upgradePopUp.transform.Find("UpgradeVideoPlayer").GetComponent<VideoPlayer>();
         panelTransform = upgradePopUp.GetComponent<RectTransform>();
         
-        GameObject input = upgradePopUp.transform.Find("Input").gameObject;
+        GameObject input = tutorialBody.transform.Find("Input").gameObject;
 
         inputIcon = input.transform.Find("InputIcon").GetComponent<Image>();
         inputText = input.transform.Find("InputText2").GetComponent<TMP_Text>();
+
+        tabText = upgradePopUp.transform.Find("TopBar").Find("System Update").GetComponent<TMP_Text>();
+
+        director = upgradePopUp.GetComponent<PlayableDirector>();
 
         hidePanel();
     }
@@ -67,6 +82,7 @@ public class TutorialPopupManager : MonoBehaviour
             if (d.option == option)
             {
                 data = d.data;
+                curData = data;
                 break;
             }
         }
@@ -83,7 +99,6 @@ public class TutorialPopupManager : MonoBehaviour
         inputText.text = "to " + data.inputName + "!";
 
         StartCoroutine(movePanel(hiddenPosition, showingPosition));
-        upgradeVideo.Play();
     }
 
     private void hidePanel()
@@ -97,6 +112,10 @@ public class TutorialPopupManager : MonoBehaviour
         float timer = 0;
         float time = moveTime;
 
+        StartCoroutine(namePanelEffect());
+
+        if (PlayerStateManager.Instance.getState().wantsTimer) TimeTextManager.Instance.hideTimer();
+
         while (timer < time)
         {
             timer += Time.deltaTime;
@@ -109,5 +128,69 @@ public class TutorialPopupManager : MonoBehaviour
 
             yield return wait;
         }
+    }
+
+    private IEnumerator namePanelEffect()
+    {
+        float waitTime = 0.5f;
+
+        float dotTime = 0.75f;
+        StartCoroutine(writeToPanel(". . .", dotTime));
+        yield return new WaitForSecondsRealtime(dotTime + 1.5f * waitTime);      
+
+        float sysUpdTime = 0.5f;
+        StartCoroutine(writeToPanel("SYSTEM UPDATE", sysUpdTime));
+        yield return new WaitForSecondsRealtime(sysUpdTime + waitTime);
+
+        float upgTime = 0.5f;
+        StartCoroutine(writeToPanel(curData.topText.ToUpper(), upgTime));
+        yield return new WaitForSecondsRealtime(upgTime + waitTime);   
+
+        director.playableAsset = show;
+        director.Play();
+        upgradeVideo.Play();
+
+        StartCoroutine(waitToHide());
+        yield break;
+    }
+
+    private IEnumerator writeToPanel(string str, float time)
+    {
+        char[] cArr = str.ToCharArray();
+        float timePerChar = time/cArr.Length;
+
+        float timer = 0;
+        int i = 0;
+
+        string s = "";
+
+        while (i < cArr.Length)
+        {
+            if (timer < timePerChar) timer += Time.deltaTime;
+            else 
+            {
+                s += cArr[i];
+                tabText.text = s;
+                ++i;
+
+                timer = 0;
+            }
+
+            yield return wait;
+        }
+    }
+
+    private IEnumerator waitToHide()
+    {
+        float tutorialTime = 4f;
+        yield return new WaitForSecondsRealtime(tutorialTime);
+
+        director.playableAsset = hide;
+        director.Play();
+        
+        if (PlayerStateManager.Instance.getState().wantsTimer) TimeTextManager.Instance.showTimer();
+
+        yield return new WaitForSecondsRealtime(1f);
+        hidePanel();
     }
 }
